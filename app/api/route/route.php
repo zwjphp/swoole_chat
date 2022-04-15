100dc536d80170357146585e62d856491084732b
aaa <834320733@qq.com>
2022-04-15 18:54:23 +0800

    登录路由添加

--- app/api/route/route.php
+++ app/api/route/route.php
@@ -3,5 +3,5 @@ use think\facade\Route;
 
 Route::group('User', function () {
     Route::rule('register', '/api/User/register','POST');
-    Route::rule('register', '/api/User/register','GET');
+    Route::rule('login', '/api/User/login','POST');
 });
